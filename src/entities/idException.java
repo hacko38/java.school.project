1ccadaffbb9fbf22cbdaab5af43d8ef5e2ddf369
Hakim <hakimdev38@gmail.com>
2018-03-04 15:37:51 +0100

    Creation de la classe idexception

---
+++ src/entities/idException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Hakim
+ */
+public class idException extends Exception {
+
+    public idException() {
+        super("Identifiant ou mot de passe non valide !");
+    }
+    
+}
